3f3e51e1d4eb772bed9d5be3b72a4cdc4f7adc5d
amulinge <41160855+amulinge@users.noreply.github.com>
2018-07-18 14:52:28 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,11 @@
+$(".garage-door-opener").click(function() {
+    $(".garage-door").slideUp();
+});
+
+$(".pokeball").click(function() {
+    $(".pikachu").fadeIn();
+});
+
+$(".light-switch").click(function() {
+    $(".light-bulb").toggle();
+});
